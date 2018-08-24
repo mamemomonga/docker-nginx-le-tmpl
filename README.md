8d937fdb88322ca5c36a006192d770fedb61e3ab
mamemomonga <mamemomonga@gmail.com>
2018-08-25 00:31:54 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,71 @@
+# ngnix docker テンプレート
+
+docker-compose で nginx を起動し、公開する設定テンプレートです。
+
+Let's EncryptによるWeb認証による証明書取得機能つきです。
+
+nginx.conf が /etc/nginx/nginx.conf, conf.d が /etc/nginx/conf.d としてマウントされます。
+
+アクセスログ・エラーログは log ディレクトリに保存されます。
+
+# 必要なもの
+
+	* 外部に公開できるサーバ
+	* Docker
+	* docker-compose
+
+### 起動
+
+	$ docker-compose up -d
+
+### 停止
+
+	$ docker-compose down
+
+# Let's Encrypt の Web認証
+
+事前にサーバへドメイン名を割り当てておく(A レコードにサーバのIPアドレスを設定)必要があります。
+
+以下のスクリプトを次のように実行すると、nginxの設定を生成しながらWeb認証を完了することができます。
+
+example.com のところをドメイン名、name@example.com をメールアドレスに置き換えてください。
+メールアドレスはLet's Encryptに送信されます。
+
+	$ ./letsencrypt-web-auth.sh example.com name@example.com
+
+以下のプロンプトがでたら、内容を確認して回答してください。
+
+	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	Please read the Terms of Service at
+	https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf. You must
+	agree in order to register with the ACME server at
+	https://acme-v02.api.letsencrypt.org/directory
+	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	(A)gree/(C)ancel: A
+	
+	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	Would you be willing to share your email address with the Electronic Frontier
+	Foundation, a founding partner of the Let's Encrypt project and the non-profit
+	organization that develops Certbot? We'd like to send you email about our work
+	encrypting the web, EFF news, campaigns, and ways to support digital freedom.
+	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	(Y)es/(N)o: Y
+
+Congratulations! Your certificate and chain have been saved at... と表示されれば取得成功です。
+
+取得に成功すると ./letsencrypt/etc/live/[DOMAIN_NAME]/ 以下に証明書が保存されます。
+
+証明書は提示更新しないと90日で失効します。
+
+更新スクリプトとして letsencrypt/update.sh, cron設定として letsencrypt/cron が生成されますので、以下の方法でcronに登録してください。
+
+月に一回強制的に更新が実行されます。
+
+	$ sudo cp letsencrypt/cron /etc/cron.d/letsencrypt
+	$ sudo chown root:root /etc/cron.d/letsencrypt
+	$ sudo chmod 644 /etc/cron.d/letsencrypt
+
+# License
+
+MIT
+
---
+++ letsencrypt-web-auth.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+set -eu
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LETSENCRYPT_DIR="$BASEDIR/letsencrypt"
+
+cd $BASEDIR
+
+function usage {
+	echo "USAGE: $0 DOMAIN_NAME ADMIN_EMAIL"
+	exit 1
+}
+
+DOMAIN_NAME="${1:-}"
+ADMIN_EMAIL="${2:-}"
+
+if [ -z "${DOMAIN_NAME:-}" ]; then
+	echo "DOMAIN_NAME not set"
+	usage
+fi
+
+if [ -z "${ADMIN_EMAIL:-}" ]; then
+	echo "ADMIN_EMAIL not set"
+	usage
+fi
+
+# if [ -e "./conf.d/$DOMAIN_NAME.conf" ]; then
+# 	echo "$DOMAIN_NAME.conf already exists."
+# 	exit 1
+# fi
+
+echo "DOMAIN_NAME: $DOMAIN_NAME"
+echo "ADMIN_EMAIL: $ADMIN_EMAIL"
+echo
+
+
+
+mkdir -p $LETSENCRYPT_DIR/webroot/$DOMAIN_NAME/webroot
+
+cat > conf.d/$DOMAIN_NAME.conf << EOS
+server {
+	listen 80;
+	server_name $DOMAIN_NAME;
+	location /.well-known {
+		root /letsencrypt/webroot/$DOMAIN_NAME;
+	}
+	location / {
+		return 404;
+	}
+}
+EOS
+
+docker-compose down
+docker-compose up -d
+
+docker run --rm -it \
+	-v $LETSENCRYPT_DIR/webroot:/webroot/$DOMAIN_NAME \
+	-v $LETSENCRYPT_DIR/etc:/etc/letsencrypt \
+	certbot/certbot certonly \
+	-d $DOMAIN_NAME \
+	-m $ADMIN_EMAIL \
+	--webroot --webroot-path /webroot
+
+cat > conf.d/$DOMAIN_NAME.conf << EOS
+server {
+	listen 80;
+	server_name $DOMAIN_NAME;
+	location /.well-known {
+		root /letsencrypt/webroot/$DOMAIN_NAME;
+	}
+	location / {
+		return 301 https://\$host\$request_uri;
+	}
+}
+server {
+	listen 443 ssl http2;
+#	listen [::]:443 ssl http2;
+	server_name $DOMAIN_NAME;
+
+	ssl_protocols TLSv1.2;
+	ssl_ciphers HIGH:!MEDIUM:!LOW:!aNULL:!NULL:!SHA;
+	ssl_prefer_server_ciphers on;
+	ssl_session_cache shared:SSL:10m;
+
+	ssl_certificate     /letsencrypt/etc/live/$DOMAIN_NAME/fullchain.pem;
+	ssl_certificate_key /letsencrypt/etc/live/$DOMAIN_NAME/privkey.pem;
+
+	location / {
+		root   /usr/share/nginx/html;
+		index  index.html index.htm;
+	}
+}
+
+EOS
+
+docker-compose down
+docker-compose up -d
+
+cat > letsencrypt/update.sh << EOS
+#!/bin/bash
+set -eux
+	cd $BASEDIR
+	docker run --rm \\
+		-v $LETSENCRYPT_DIR/webroot:/webroot \\
+		-v $LETSENCRYPT_DIR/etc:/etc/letsencrypt \\
+		certbot/certbot renew --force-renew
+	/usr/local/bin/docker-compose down
+	/usr/local/bin/docker-compose up -d
+EOS
+
+chmod 755 letsencrypt/update.sh
+echo "Write: letsencrypt/update.sh"
+perl -e "printf('%02d %02d %02d * * $(whoami) $BASEDIR/letsencrypt/update.sh'.qq{\n},int(rand()*59),3,int(rand()*30)+1)" > $BASEDIR/letsencrypt/cron
+
+echo "Write: letsencrypt/cron"
+
+echo
+echo " *** SERVER IS READY ***"
+echo "   https://$DOMAIN_NAME/"
+
